fceb7112b9e123f2d863ca1c02feb2d1f4be656b
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-13 11:30:05 +0530

    Variables in php

---
+++ php-learning/3_Variables.php
@@ -0,0 +1,10 @@
+// variables in php are always declared using $ sign 
+
+<?php
+
+    $fruit = 'Apple';  // String type variable
+    echo $fruit;
+
+    $sum = 8+9;  // integer type variable
+    echo $sum;
+  ?>
