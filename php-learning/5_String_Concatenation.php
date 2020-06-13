3b94b5aa20249638cc9d37985e48408f96bbba37
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-13 11:51:10 +0530

    String Concatenation in php

---
+++ php-learning/5_String_Concatenation.php
@@ -0,0 +1,10 @@
+// String Concatenation in php happens using the . for instance  echo 'hello'.' world' => hello world
+// for variables we only need to include {$var_name}
+
+<?php
+
+    $name = 'Ken the Ninja';
+    // Output "Hello, ____" using the $name variable. 
+    echo "Hello, {$name}";
+    
+  ?>
