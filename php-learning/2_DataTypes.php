ec4c808a80ecf2b5fe74f6a8c40dcfdab8a492ba
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-13 11:21:21 +0530

    Data types in php

---
+++ php-learning/2_DataTypes.php
@@ -0,0 +1,24 @@
+/* DATA TYPES IN PHP
+*  Strings 
+*  integers
+*  floating values
+*  double values
+*  and so on there are datatypes in php same as other languages
+*/
+
+<?php
+
+// integer in php
+echo 3;
+
+// String in php
+echo 'Hello';
+
+// Different data types in php
+// simple add for integers
+echo 30+40;    // 70
+
+// the add with ' ' makes it a String
+echo '30+40';  // 30+40
+
+?>
