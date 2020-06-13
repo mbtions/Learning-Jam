37200cbad43c0cdcdb08719fc4f3b20bd5a4aa83
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-13 12:05:29 +0530

    If Statement in php

---
+++ php-learning/6_If_Statement.php
@@ -0,0 +1,20 @@
+// If statement is the satement that checks whether the condition is true or not and then only it gets executed 
+/* Syntax :
+ *           if(codition){
+ *                statements;
+ *           } 
+ *               if true then execute if block statements else skip
+ */        
+<?php
+    $x = 99 * 99;
+    $y = 77 * 77;
+
+    // Write the if statements below
+    if($x > 9800){ 
+      echo 'The $x variable is greater than 9800.';
+    }
+    
+    if($y > 6000){
+      echo 'The $y variable is greater than 6000.';
+    }
+  ?>
