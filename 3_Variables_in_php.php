f84b6fb2541eb153109dd5958968f4bf70b17b52
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-15 19:35:21 +0530

    added 3 Variables in php

---
+++ 3_Variables_in_php.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  /* variables in php are always declared using $ sign */
+
+<?php
+
+    $fruit = 'Apple';  /* String type variable */
+    echo $fruit;
+
+    $sum = 8+9;  /* integer type variable */
+    echo $sum;
+  ?>
+
+  <!-- Declare the variable $fruit below -->
+  <?php
+    $fruit = 'Apple';
+    echo $fruit;
+  ?>
+
+  <br>
+
+  <!-- Declare the variable $sum below -->
+  <?php
+    $sum = 8+9;
+    echo $sum;
+  ?>
+
+</body>
+</html>
