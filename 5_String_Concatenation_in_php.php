0c4ba5b34520ab917a56ab2b3605def3d093d2a1
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-15 19:37:02 +0530

    added String Concatenation in php

---
+++ 5_String_Concatenation_in_php.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+/* String Concatenation in php happens using the . for instance  echo 'hello'.' world' => hello world
+ * for variables we only need to include {$var_name} within ' '
+ */
+
+  <?php
+
+   // for two String concatenation using .
+   $str1 = 'Hello';
+   $str2 = 'World';
+
+   $str = str1.str2;
+   echo str;
+
+   // for variables within ' ' using {$var}
+   $name = 'Ken the Ninja';
+
+   // Output "Hello, ____" using the $name variable. 
+   echo "Hello, {$name}";
+
+  ?>
+
+</body>
+</html>
