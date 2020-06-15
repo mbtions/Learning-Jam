329f02a471b7bcb73c1d61e19f8d03e4f9e13fb5
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-15 19:34:34 +0530

    added 2 Data Types in php

---
+++ 2_Data_Types_in_php.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+/* DATA TYPES IN PHP
+*  Strings 
+*  integers
+*  floating values
+*  double values
+*  and so on there are datatypes in php same as other languages
+*/
+
+<?php
+
+// integer in php
+echo 3;
+
+// String in php
+echo 'Hello';
+
+// Different data types in php
+// simple add for integers
+echo 30+40;    // 70
+
+// the add with ' ' makes it a String
+echo '30+40';  // 30+40
+
+?>
+  <!-- Output 5 + 7 below -->
+  <?php
+    echo 5+7;
+  ?>
+
+  <br>
+
+  <!-- Output '5 + 7' below -->
+  <?php
+    echo '5+7';
+  ?>
+
+</body>
+</html>
