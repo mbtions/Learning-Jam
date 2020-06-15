5fb4235993b0e6a2ab5fac5e1dc50769107165f9
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-15 19:37:58 +0530

    added 6 If Statement in php

---
+++ 6_If_Statement_in_php.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+/* If statement is the satement that checks whether the condition is true or not and then only it gets executed 
+ * Syntax :
+ *           if(codition){
+ *                statements;
+ *           } 
+ *               if true then execute if block statements else skip
+ */        
+<?php
+
+    $x = 99 * 99;
+    $y = 77 * 77;
+
+    // Write the if statements below
+    if($x > 9800){ 
+      echo 'The $x variable is greater than 9800.';
+    }
+    
+    if($y > 6000){
+      echo 'The $y variable is greater than 6000.';
+    }
+ 
+  ?>
+
+</body>
+</html>
